077410813e9b1e2c541d1cda6c0ded8254f8e01a
mongchanghsi <m.changhsi@gmail.com>
2022-05-15 21:34:26 +0800

    Add WaleltLink connector and functions for RPC URL

--- connectors/index.ts
+++ connectors/index.ts
@@ -1,13 +1,48 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { WalletLinkConnector } from '@web3-react/walletlink-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+
 import envConfig from '../utils/envConfig';
 
+const checkNetworkName = () => {
+  if (
+    typeof window !== 'undefined' &&
+    window.location.hostname &&
+    window.location.hostname === 'localhost'
+  ) {
+    return 31337;
+  } else if (envConfig.MAINNET) {
+    return 137;
+  } else {
+    return 80001;
+  }
+};
+
+const generateRPCUrl = (chainId: number) => {
+  switch (chainId) {
+    case 137:
+      return `https://polygon-mainnet.infura.io/v3/${envConfig.INFURA_ID}`;
+    case 80001:
+      return `https://polygon-mumbai.infura.io/v3/${envConfig.INFURA_ID}`;
+    case 31337:
+      return `https://polygon-mumbai.infura.io/v3/${envConfig.INFURA_ID}`;
+    default:
+      return `https://polygon-mumbai.infura.io/v3/${envConfig.INFURA_ID}`;
+  }
+};
+
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 4, 31337, 137],
+  supportedChainIds: [31337, 137, 80001],
 });
 
 export const coinbaseWallet = new WalletLinkConnector({
-  url: `https://mainnet.infura.io/v3/${envConfig.INFURA_ID}`,
+  url: generateRPCUrl(checkNetworkName()),
   appName: 'SafeZen',
-  supportedChainIds: [1, 4, 31337, 137],
+  supportedChainIds: [31337, 137, 80001],
+});
+
+export const walletlink = new WalletLinkConnector({
+  url: generateRPCUrl(checkNetworkName()),
+  appName: 'SafeZen',
+  supportedChainIds: [31337, 137, 80001],
 });
